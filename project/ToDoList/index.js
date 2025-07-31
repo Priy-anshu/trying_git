b2e5e58f012d3649f03553828014f8aba8ee80b6
Priyanshu patel <Priyanshu102938@gmail.com>
2025-07-31 15:45:34 +0530

    "normal to do list with functions is created"

--- project/ToDoList/index.js
+++ project/ToDoList/index.js
@@ -13,10 +13,14 @@ setTime();
 // adding new taks
 const addNewList=document.getElementById("add");
 
-addNewList.addEventListener("click" , () => {
-// if input box is already open and 
-// you are clicking on add new list again
-// so just return
+addNewList.addEventListener("click" , showInputField);
+
+
+// function to create and show input field so it can take input
+function showInputField() {
+    // if input box is already open and 
+    // you are clicking on add new list again
+    // so just return
     if(document.querySelector("#takingInput"))
         return ;
 
@@ -32,46 +36,57 @@ addNewList.addEventListener("click" , () => {
     // todayDate.insertAdjacentElement("beforebegin",inputConfirm );
     todayDate.before(input,inputConfirm);
 
-// data if filled and want to add into list
-    inputConfirm.addEventListener("click", () => {
-        let item= document.createElement("p");
-        const justHolding=input.value.trim();
-        item.textContent=justHolding;
+    // data if filled and want to add into list
+    inputConfirm.addEventListener("click", () => addTask(input,inputConfirm));
+    
+};
 
-// if input area is empty dont create any thing
-        if(!justHolding) {
-            input.remove();
-            inputConfirm.remove();
-            return;
-        }
 
-        let taskComplete=document.createElement("input");
-        taskComplete.type="checkbox";
 
-        
+//function if data is filled and want to add into list
+function addTask(input,inputConfirm) {
+    const justHolding=input.value.trim();
 
-        let taskDelete=document.createElement("button");
-        taskDelete.textContent="delete";
-        
-        
-        let newList=document.createElement("li");
-
-// add into list first for better reach while deletion
-        newList.append(item,taskDelete,taskComplete);
-
-// add on the wabsite
-        document.getElementById("taskList").append(newList);
-        
-// want to delete the added task
-        taskDelete.addEventListener("click" , () => {
-
-// to delete the closest li tag from where the button was clicked
-            taskDelete.closest("li").remove();
-        });
-
-// remove input area and button once task is added
+    // if input area is empty dont create any thing
+    if(!justHolding) {
         input.remove();
         inputConfirm.remove();
-    });
+        return;
+    }
+
+    createTaskElement(justHolding);
+
+    // remove input area and button once task is added
+    input.remove();
+    inputConfirm.remove();
+};
+
+
+//function to delete the closest li tag from where the button was clicked
+function deleteTask (taskDelete){
+    taskDelete.closest("li").remove();
+};
+
+
+// function to create the formate in which the tasks will be shown
+function createTaskElement(justHolding) {
+    const item= document.createElement("p");
+    item.textContent=justHolding;
+
+    const taskComplete=document.createElement("input");
+    taskComplete.type="checkbox";
+
+    const taskDelete=document.createElement("button");
+    taskDelete.textContent="delete";
     
-});
+    const newList=document.createElement("li");
+
+    // add into list first for better reach while deletion
+    newList.append(item,taskDelete,taskComplete);
+
+    // add on the wabsite
+    document.getElementById("taskList").append(newList);
+
+    // want to delete the added task
+    taskDelete.addEventListener("click" , () => deleteTask(taskDelete)) ;
+};
